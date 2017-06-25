afcf48766dee865af6a5969a5bb95a407b4a6153
dhruvramani <dhruvramani98@gmail.com>
2017-06-25 22:24:11 +0530

    Authentication Added

--- IG_Connect/IG_Connect/urls.py
+++ IG_Connect/IG_Connect/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include 
 from django.contrib import admin
+import authentication.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', authentication.views.index),
+    url(r'^index/', authentication.views.index),
+    url(r'^auth/', include('authentication.urls', namespace = 'authentication')),
+    url(r'^projects/', include('projects.urls', namespace = 'projects'))
 ]
