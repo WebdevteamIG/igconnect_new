149b7cad0fcb2f678efde178afbc74c982f7179c
priyamkhandelwal <priyamkhandelwalbtp@gmail.com>
2017-07-30 15:03:04 +0530

    adding admin static folder

--- IG_Connect/IG_Connect/settings.py
+++ IG_Connect/IG_Connect/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ('./static',)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
