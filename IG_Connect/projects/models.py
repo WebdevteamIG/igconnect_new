5f1540eded5bce1abdf7077be030c44ed43e59ec
saitejach127 <code@DESKTOP-UHDGKEQ.localdomain>
2018-10-16 22:00:07 +0530

    projects changes

--- IG_Connect/projects/models.py
+++ IG_Connect/projects/models.py
@@ -15,8 +15,8 @@ class Project(models.Model) :
 	user = models.ForeignKey(User,on_delete=models.CASCADE)
 	projectName = models.CharField(max_length=200)
 	teampic = models.ImageField(upload_to=get_team_pic_path,null=True,blank=True)
-	shortDesc = models.CharField(max_length=100)
-	objective = models.TextField(max_length=1000)
+	shortDesc = models.CharField(max_length=1000)
+	objective = models.TextField(max_length=2000)
 	drivingForce = models.TextField(max_length=1000)
 	publishedDate = models.DateField(auto_now=False, auto_now_add=False)
 	branchList = models.CharField(max_length=200,null=True)
@@ -25,7 +25,7 @@ class Project(models.Model) :
 	impact = models.TextField(max_length=500)
 	awards = models.TextField(max_length=1000)
 	plans = models.TextField(max_length=1000)
-	projecturl = models.URLField(max_length=500,default=None)
+	projecturl = models.URLField(default=None)
 
 	def __str__(self) : 
 		return self.projectName
