2f21adf4c26d21b51c3454d95167d676f9ac813e
saitejach127 <saitejach127@gmail.com>
2019-03-27 21:14:11 +0530

    hardware shop added

--- IG_Connect/IG_Connect/urls.py
+++ IG_Connect/IG_Connect/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 
         auth_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    url(r'^hardware/',include('hardware_shop.urls', namespace='hardware_shop')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
---
+++ IG_Connect/hardware_shop/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$',views.index),
+	url(r'^buynow/$', views.buynow),
+	url(r'^lend/$', views.lend),
+	url(r'^lended/$', views.lendedComponents),
+]
